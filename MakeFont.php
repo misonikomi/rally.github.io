cebc6c71cfae5b5564265b1761e36f24f179cc7e
4k170 <4k170.me@gmail.com>
2017-06-16 16:21:40 +0900

    2017.06.16
    Websites

---
+++ MakeFont.php
@@ -0,0 +1,13 @@
+<?php
+
+$read = file_get_contents($argv[1]);
+$str = strip_tags($read);
+
+$str = str_replace("\n", "", $str);
+$str = str_replace("", "", $str);
+$str = str_replace(" ", "", $str);
+$str = str_replace("\t", "", $str);
+
+$fp = fopen("needs.txt", "a");
+fwrite($fp, $str);
+fclose($fp);
